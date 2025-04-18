aa302301bac1acffab04902ff79f298710454b6f
UnnamedBruh <165609225+UnnamedBruh@users.noreply.github.com>
2025-04-18 11:36:29 -0500

    Update otherEffects.js

--- otherEffects.js
+++ otherEffects.js
@@ -30,4 +30,30 @@
 	barcodeFont.load().then(function(loadedFont) {
 		document.fonts.add(loadedFont);
 	});
+
+	const random = document.createElement("option");
+	random.value = "rand";
+	random.textContent = "Noise";
+
+	function noise(len, imageData, currentImageData, x) {
+		if (effectParams[0].value == 0) return;
+		const perc = +effectParams[0].value / 100;
+		let i = 0;
+		const imgD = new ImageData(new Uint8ClampedArray(len), currentImageData.width, currentImageData.height);
+		const data = imgD.data;
+
+		for (; i < len;) {
+			data[i] = interpolate(imageData[i], Math.random() * 255, perc); i++;
+			data[i] = interpolate(imageData[i], Math.random() * 255, perc); i++;
+			data[i] = interpolate(imageData[i], Math.random() * 255, perc); i++;
+			data[i] = imageData[i]; i++;
+		}
+		x(imgD);
+	}
+
+	customModdedEffects.push(["rand", "<a>Progress (as a percentage):</a><input type=\"number\" value=\"100\" step=\"0.390625\" min=\"0\" max=\"100\" id=\"e-invpro\">%", function() {
+		return [document.getElementById("e-invpro")];
+	}, noise]);
+
+	effects.appendChild(random);
 })();
